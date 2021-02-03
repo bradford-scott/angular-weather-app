7a90c61c4662ef307477cf73d4604116c4dab1d1
Bradford Scott - 255441 <bradford.scott@centene.com>
2021-02-02 20:16:51 -0600

    working version with city only

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApixuService } from '../apixu.service';
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
+  public weatherData: any;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apixuService: ApixuService
+  ) { }
 
   ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: ['']
+    });
   }
 
+  sendToAPIXU(formValues) {
+    this.apixuService
+      .getWeather(formValues.location)
+      .subscribe(data => this.weatherData = data);
+    console.log(this.weatherData);
+  }
 }
