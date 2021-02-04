d9c65dc8a218b678bda15e5ae6802f11f5a49525
Bradford Scott - 255441 <bradford.scott@centene.com>
2021-02-04 09:29:02 -0600

    incomplete zip/country code implementation

--- src/app/openweathermap.service.ts
+++ src/app/openweathermap.service.ts
@@ -23,10 +23,18 @@ export class OpenweathermapService {
     );
   }
 
+  getWeatherByZipcode(values) {
+    return this.http.get(
+      'https://api.openweathermap.org/data/2.5/weather?zip=' + values.zipCode + ',' + values.countryCode + '&appid={API key}'
+      // 'https://api.openweathermap.org/data/2.5/weather?lat=' + values.locationByLat + '&lon=' + values.locationByLon +
+      // '&units=imperial&appid=cf002751564a4c78f5f7ed479f1b9ba3'
+    );
+  }
+
   getWeatherByLatAndLon(values) {
     return this.http.get(
       'https://api.openweathermap.org/data/2.5/weather?lat=' + values.locationByLat + '&lon=' + values.locationByLon +
-      '&appid=cf002751564a4c78f5f7ed479f1b9ba3'
+      '&units=imperial&appid=cf002751564a4c78f5f7ed479f1b9ba3'
     );
   }
 }
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { OpenweathermapService } from '../openweathermap.service';
 
 @Component({
@@ -16,6 +16,39 @@ export class WeatherComponent implements OnInit {
     locationByLat: new FormControl(''),
     locationByLon: new FormControl(''),
   });
+
+  websiteList: any = ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG',
+    'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BH', 'BS', 'BD', 'BB', 'BY', 'BE', 'BZ',
+    'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF',
+    'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO',
+    'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ',
+    'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'ET', 'FK', 'FO', 'FJ', 'FI',
+    'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD',
+    'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU',
+    'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO',
+    'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY',
+    'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ',
+    'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM',
+    'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MP', 'NO',
+    'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR',
+    'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS',
+    'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO',
+    'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SZ', 'SE', 'CH', 'SY', 'TW',
+    'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT' , 'TN', 'TR', 'TM', 'TC', 'TV',
+    'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI',
+    'WF', 'EH', 'YE', 'ZM', 'ZW'];
+
+  form = new FormGroup({
+    website: new FormControl('', Validators.required)
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
 
   constructor(
     private formBuilder: FormBuilder,
