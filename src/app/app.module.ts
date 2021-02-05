8c73600883a771dbeb3e8225e3439182cfb18e2e
Bradford Scott - 255441 <bradford.scott@centene.com>
2021-02-05 14:32:34 -0600

    changed karma to run headless, minor refactoring, added single unit test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import {RouterModule} from '@angular/router';
 import {allAppRoutes} from './routes';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { OpenweathermapService } from './openweathermap.service';
 
@@ -19,6 +19,7 @@ import { OpenweathermapService } from './openweathermap.service';
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(allAppRoutes),
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule
   ],
--- src/app/weather/weather.component.spec.ts
+++ src/app/weather/weather.component.spec.ts
@@ -1,21 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WeatherComponent } from './weather.component';
+import { OpenweathermapService } from '../openweathermap.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
   let fixture: ComponentFixture<WeatherComponent>;
+  let openWeatherMapServiceSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WeatherComponent ]
+      imports: [
+        ReactiveFormsModule,
+      ],
+      declarations: [ WeatherComponent ],
+      providers: [
+        {
+          provide: OpenweathermapService, useValue: {
+            getWeather: () => {}
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WeatherComponent);
+    openWeatherMapServiceSpy = TestBed.get(OpenweathermapService);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  describe('sendCityToOpenWeatherMap', () => {
+    fit('should make a call to openWeatherMapService getWeather function, submit locationByCity and retrieve weatherData', () => {
+      spyOn(openWeatherMapServiceSpy, 'getWeather').and.returnValue({ subscribe: () => {}});
+      component.sendCityToOpenWeatherMap( { locationByCity: 'STL' });
+      expect(openWeatherMapServiceSpy.getWeather).toHaveBeenCalledWith('STL');
+    });
   });
 });
