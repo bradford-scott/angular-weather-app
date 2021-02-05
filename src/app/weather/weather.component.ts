3b1440384648c821231834f10fb7ae88b3257a8f
Bradford Scott - 255441 <bradford.scott@centene.com>
2021-02-05 14:36:49 -0600

    removed unused validator import

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { OpenweathermapService } from '../openweathermap.service';
 
 @Component({
